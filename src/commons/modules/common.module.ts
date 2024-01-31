5bfa313c99ad484e8614c90eaf3f26daf7d09d66
SofienRogue <sofien.abidi@digitalroguewave.com>
2024-01-31 19:47:10 +0100

    update import config + package version

--- src/commons/modules/common.module.ts
+++ src/commons/modules/common.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { IsExist } from '../general';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot(),
+  ],
   providers: [IsExist],
 })
 export class CommonModule {}
