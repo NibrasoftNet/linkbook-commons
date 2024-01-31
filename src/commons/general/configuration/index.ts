6e16fbc91fa2ca3b97cc2e511e2eb0ed1611ce65
SofienRogue <sofien.abidi@digitalroguewave.com>
2024-01-31 08:21:20 +0100

    update import config + package version

--- src/commons/general/configuration/index.ts
+++ src/commons/general/configuration/index.ts
@@ -1,6 +1,8 @@
-export * as appConfig from "./app.config";
-export * as databaseConfig from "./database.config";
-export * as authConfig from "./auth.config";
+import appConfig from "./app.config";
+import databaseConfig from "./database.config";
+import authConfig from "./auth.config";
+
+export { appConfig as appConfiguration,  authConfig as authConfigConfiguration, databaseConfig as databaseConfiguration}
 
 export * from './auth-config.type'
 export * from './app-config.type'
