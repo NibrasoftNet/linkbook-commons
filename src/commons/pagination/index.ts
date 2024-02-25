8080ccbcc69e7cae342130fede7e87c1d4c1cb80
SofienRogue <sofien.abidi@digitalroguewave.com>
2024-02-25 21:29:35 +0100

    add pagination

--- src/commons/pagination/index.ts
+++ src/commons/pagination/index.ts
@@ -1,5 +1,5 @@
 export * from './filter.input';
-export * from './pagination-args.dto';
 export * from './paginate-config.interface';
 export * from './paginate-util';
 export * from './pagination.interface';
+export * from './pagination-args.dto';
